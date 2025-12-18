ee905127522dc7413d7aa928a73ac30408da304f
uvindukaralliyadda <134487180+uvindukaralliyadda@users.noreply.github.com>
2025-12-18 20:48:27 +0530

    about section

--- src/Components/AboutSection/AboutSection.jsx
+++ src/Components/AboutSection/AboutSection.jsx
@@ -10,7 +10,7 @@ const AboutSection = () => {
         transition={{ duration: 2 }}>
       <motion.div className='abouts' initial={{ opacity: 0 }} whileInView={{opacity: 1 }} transition={{ duration: 2 }}>
         <h2>About Company</h2>
-        <p>Reyaan Ventures Private Limited is a leading investments facilitator firm based in the Sri Lanka. We specialize in facilitating foreign investment into the country by connecting potential investors with government and private sector projects. Our strong relationships with key government officials and a proven track record of successful investment deals within a short period of our existence, have earned us the trust and confidence of both government and private companies.</p>
+        <p>Reyaan Ventures Private Limited is a leading investments facilitator with a firm based in Sri Lanka. We specialize in facilitating foreign investment into the country by connecting potential investors with private sector projects. Our strong relationships with key government and private officials with a proven track record of successful investment negotiations within a short period of our existence, have earned us the trust and confidence of both government and private companies.</p>
 
       </motion.div>
       <motion.div className='abouts2' initial={{ opacity: 0 }} whileInView={{opacity: 1 }} transition={{ duration: 2 }}>
