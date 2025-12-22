fe1266dc28ba3ceeb80e0b5127c05a90c7795c66
uvindukaralliyadda <134487180+uvindukaralliyadda@users.noreply.github.com>
2025-12-22 12:23:23 +0530

    profile fixed with about section

--- src/Components/AboutSection/AboutSection.jsx
+++ src/Components/AboutSection/AboutSection.jsx
@@ -10,7 +10,7 @@ const AboutSection = () => {
         transition={{ duration: 2 }}>
       <motion.div className='abouts' initial={{ opacity: 0 }} whileInView={{opacity: 1 }} transition={{ duration: 2 }}>
         <h2>About Company</h2>
-        <p>Reyaan Ventures Private Limited is a leading investments facilitator with a firm based in Sri Lanka. We specialize in facilitating foreign investment into the country by connecting potential investors with private sector projects. Our strong relationships with key government and private officials with a proven track record of successful investment negotiations within a short period of our existence, have earned us the trust and confidence of both government and private companies.</p>
+        <p>Reyaan Ventures Private Limited is a leading investments facilitator with a firm based in Sri Lanka & Bangladesh. We specialize in facilitating foreign investment into the countries by connecting potential investors with private sector collaborations and join venture. Our strong relationships with key personnel with a proven track record of successful investment negotiations within a short period of our existence, have earned us the trust and confidence of different sectors.</p>
 
       </motion.div>
       <motion.div className='abouts2' initial={{ opacity: 0 }} whileInView={{opacity: 1 }} transition={{ duration: 2 }}>
